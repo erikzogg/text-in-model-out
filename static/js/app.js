34c882dd57b7b360df8a721ec29d79849212bc31
Erik Zogg <50068419+erikzogg@users.noreply.github.com>
2021-04-04 17:12:40 +0200

    :sparkles: Add API request

--- static/js/app.js
+++ static/js/app.js
@@ -14,4 +14,41 @@ let initApp = function () {
             document.getElementById('button-create-model').setAttribute('disabled', 'disabled');
         }
     });
+
+    let buttonCreateModel = document.getElementById('button-create-model');
+
+    buttonCreateModel.addEventListener('click', function () {
+        this.setAttribute('disabled', 'disabled');
+        this.innerHTML = 'Please wait...';
+
+        let formData = new FormData();
+        formData.append('text', document.getElementById('process-description').value);
+
+        const request = new Request('/api', {
+            headers: {'X-CSRFToken': document.getElementById('csrf_token').value},
+            method: 'POST',
+            body: formData
+        });
+
+        fetch(request)
+            .then()
+            .then(response => {
+                this.innerHTML = 'Create Process Model';
+                this.removeAttribute('disabled');
+
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+
+                return response.text();
+            })
+            .then(html => handleResponse(html))
+            .catch(error => {
+                document.getElementById('process-model').innerHTML = error;
+            });
+    })
+};
+
+let handleResponse = function (data) {
+    document.getElementById('process-model').innerHTML = data;
 };
