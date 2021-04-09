7be3366ce93c50e95e33c47093f1426d8e24ba55
Erik Zogg <50068419+erikzogg@users.noreply.github.com>
2021-04-09 16:55:53 +0200

    :construction: Improve start event detection

--- backend/nlp.py
+++ backend/nlp.py
@@ -7,6 +7,8 @@ determiners = ['a', 'an', 'the']
 
 
 def parse(text):
+    output = {"startevent": "", "activities": [], "endevent": ""}
+
     activities = []
 
     nlp = spacy.load("en_core_web_sm")
@@ -14,56 +16,36 @@ def parse(text):
 
     doc = nlp(text)
 
-    startevent = get_startevent(doc)
-    print(startevent)
-
     for index, sent in enumerate(doc.sents):
         for token in sent:
-            if token.pos_ == "VERB":
+            # First iteration: Detect startevent
+            if token.pos_ == "VERB" and output["startevent"] == "":
+                for child in token.children:
+                    if child.dep_ == "nsubjpass" or child.dep_ == "dobj":
+                        startevent = child.text + " " + token._.inflect('VBN')
+
+                        if startevent.split(' ')[0].lower() in determiners:
+                            startevent = " ".join(startevent.split(' ')[1:])
+
+                        output["startevent"] = startevent
+            # Remaining iterations: Detect activities
+            elif token.pos_ == "VERB":
                 if len([child for child in token.children if (child.pos_ == "VERB" and child.dep_ == "xcomp")]) > 0:
-                    # Skip semi-modal verbs
+                    # Skip semi-modal verbs (e.g. needs to...)
                     continue
 
-                activity = ""
+                activity = token.lemma_
 
                 for child in token.children:
                     # Find phrasal verbs
                     if child.pos_ == "ADP":
                         activity = token.lemma_ + " " + child.lemma_
 
-                if activity == "":
-                    activity = token.lemma_
-
-                activities.append(activity)
+                output["activities"].append(activity)
 
     svg = displacy.render(doc, style="dep")
 
     output_path = Path("./dependency_plot.svg")
     output_path.open("w", encoding="utf-8").write(svg)
 
-    return activities
-
-
-def get_startevent(doc):
-    startevent = ""
-
-    for index, sentence in enumerate(doc.sents):
-        if index == 0:
-            for token in sentence:
-                if token.pos_ == "VERB":
-                    if len([child for child in token.children if (child.pos_ == "VERB" and child.dep_ == "xcomp")]) > 0:
-                        # Skip semi-modal verbs
-                        continue
-
-                    for child in token.children:
-                        if child.dep_ == "nsubjpass" or child.dep_ == "dobj":
-                            startevent = child.text + " " + token._.inflect('VBN')
-
-                            if startevent.split(' ')[0] in determiners:
-                                startevent = " ".join(startevent.split(' ')[1:])
-
-                            return startevent
-        else:
-            break
-
-    return startevent
+    return output
--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from backend.nlp import parse
+import json
 
 
 def index(request):
     results = parse(request.POST['text'])
 
-    return HttpResponse(str(results))
+    return HttpResponse("<pre>" + json.dumps(results, indent=4) + "</pre>")
