e488dce00d1a26d5d0ffe62308c3d627da2e6998
Erik Zogg <50068419+erikzogg@users.noreply.github.com>
2021-04-10 17:24:00 +0200

    :sparkles: Add role detection

--- backend/nlp.py
+++ backend/nlp.py
@@ -8,6 +8,7 @@ determiners = ['a', 'an', 'the']
 
 def parse(text):
     output = {"startevent": "", "activities": [], "endevent": ""}
+    result = []
 
     nlp = spacy.load("en_core_web_sm")
     nlp.add_pipe("merge_noun_chunks")
@@ -15,13 +16,25 @@ def parse(text):
     doc = nlp(text)
 
     sentences_number = len(list(doc.sents))
+    current_role = "Default"
 
     for index, sent in enumerate(doc.sents, 1):
         for token in sent:
+            # Detect role
+            if token.pos_ == "VERB":
+                role = next((child for child in token.children if (child.dep_ == "nsubj")), None)
+                if role:
+                    not_suitable = next((child for child in token.children if (child.dep_ in ["mark", "xcomp", "ccomp"])), None)
+                    if not not_suitable:
+                        rolename = role.text
+                        rolename = " ".join([word for word in rolename.split() if word.lower() not in determiners])
+
+                        current_role = rolename
+
             # First iteration: Detect startevent
             if token.pos_ == "VERB" and output["startevent"] == "":
                 the_object = next((child for child in token.children if (child.dep_ == "nsubjpass" or child.dep_ == "dobj")), None)
-                phrasal_verb = next((child for child in token.children if (child.pos_ == "ADP")), None)
+                phrasal_verb = next((child for child in token.children if (child.pos_ == "ADP" and child.dep_ == "prt")), None)
 
                 if not the_object and phrasal_verb:
                     the_object = next((child for child in phrasal_verb.children if (child.dep_ == "pobj")), None)
@@ -38,14 +51,20 @@ def parse(text):
                     startevent = " ".join([word for word in startevent.split() if word.lower() not in determiners])
 
                     output["startevent"] = startevent
+                    result.append({'type': 'startevent', 'value': startevent, 'role': current_role})
             # Remaining iterations: Detect activities
             elif token.pos_ == "VERB":
+                # Detect condition
+                has_mark = next((child for child in token.children if (child.pos_ == "ADV")), None)
+                if has_mark:
+                    continue
+
                 if len([child for child in token.children if (child.pos_ == "VERB" and child.dep_ == "xcomp")]) > 0:
                     # Skip semi-modal verbs (e.g. needs to...)
                     continue
 
                 the_object = next((child for child in token.children if (child.dep_ == "nsubjpass" or child.dep_ == "dobj")), None)
-                phrasal_verb = next((child for child in token.children if (child.pos_ == "ADP")), None)
+                phrasal_verb = next((child for child in token.children if (child.pos_ == "ADP" and child.dep_ == "prt")), None)
 
                 if not the_object and phrasal_verb:
                     the_object = next((child for child in phrasal_verb.children if (child.dep_ == "pobj")), None)
@@ -70,32 +89,43 @@ def parse(text):
                     activity = " ".join([word for word in activity.split() if word.lower() not in determiners])
 
                     output["activities"].append(activity)
+                    result.append({'type': 'activity', 'value': activity, 'role': current_role})
 
             # End event detection
             if index == sentences_number:
                 if token.pos_ == "VERB":
-                    the_object = next((child for child in token.children if (child.dep_ == "nsubjpass" or child.dep_ == "dobj")), None)
-                    phrasal_verb = next((child for child in token.children if (child.pos_ == "ADP")), None)
+                    # Make sure that only the last verb in a sentence is considered
+                    if len([child for child in token.children if (child.pos_ == "VERB")]) == 0:
+                        the_object = next((child for child in token.children if (child.dep_ == "nsubjpass" or child.dep_ == "dobj")), None)
+                        phrasal_verb = next((child for child in token.children if (child.pos_ == "ADP" and child.dep_ == "prt")), None)
 
-                    if not the_object and phrasal_verb:
-                        the_object = next((child for child in phrasal_verb.children if (child.dep_ == "pobj")), None)
+                        if not the_object and phrasal_verb:
+                            the_object = next((child for child in phrasal_verb.children if (child.dep_ == "pobj")), None)
 
-                    endevent = None
+                        if not the_object:
+                            for parent in token.ancestors:
+                                if token in parent.children and token.dep_ == "xcomp":
+                                    the_object = next(subject for subject in parent.children if (subject.dep_ == "nsubj"))
 
-                    if the_object:
-                        endevent = the_object.text + " " + token._.inflect('VBN')
+                        endevent = None
 
-                    if the_object and phrasal_verb:
-                        endevent = the_object.text + " " + token._.inflect('VBN') + " " + phrasal_verb.text
+                        if the_object:
+                            endevent = the_object.text + " " + token._.inflect('VBN')
 
-                    if endevent:
-                        endevent = " ".join([word for word in endevent.split() if word.lower() not in determiners])
+                        if the_object and phrasal_verb:
+                            endevent = the_object.text + " " + token._.inflect('VBN') + " " + phrasal_verb.text
 
-                        output["endevent"] = endevent
+                        if endevent:
+                            endevent = " ".join([word for word in endevent.split() if word.lower() not in determiners])
+
+                            output["endevent"] = endevent
+
+    if not output["endevent"] == "":
+        result.append({'type': 'endevent', 'value': output["endevent"], 'role': current_role})
 
     svg = displacy.render(doc, style="dep")
 
     output_path = Path("./dependency_plot.svg")
     output_path.open("w", encoding="utf-8").write(svg)
 
-    return output
+    return result
